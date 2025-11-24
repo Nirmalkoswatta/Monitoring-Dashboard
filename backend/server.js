4e78ba0a439b1db362fc85f225215a81b0414a1e
Nirmal koswatta <nirmalkoza@gmail.com>
2025-11-24 20:15:50 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://ci-cd-monitoring-dashboard.netlify.app'
+}));
 app.use(express.json());
 
 // Test endpoint
